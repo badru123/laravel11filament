50eaee1c03a59336d419e593d3dfbe4acfbe4054
root <root@srv570245.hstgr.cloud>
2024-09-02 12:42:12 +0000

    xommit ke 2

---
+++ app/Models/Treatment.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use App\Casts\MoneyCast;
+
+class Treatment extends Model
+{
+    use HasFactory;
+    protected $casts = [
+        'price' => MoneyCast::class,
+    ];
+    public function patient(): BelongsTo
+    {
+        return $this->belongsTo(Patient::class);
+    }
+}
